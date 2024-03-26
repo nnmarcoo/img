fe8eb30f6d7c2fd7e445993576d47cf5b9d56507
Marco <marcosteam7@gmail.com>
2024-03-26 17:05:14 -0400

    file selector

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 const { invoke } = window.__TAURI__.tauri;
+const { open } = window.__TAURI__.dialog
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+document.getElementById('fileSelect').addEventListener('click', () => {
+  const selected = open({
+    multiple: false,
+    filters: [{
+      name: 'Image',
+      extensions: ['png', 'jpeg']
+    }]
   });
+
+  if (selected !== null) {
+    // TODO: set image display
+  }
 });
