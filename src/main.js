274f2b03a319c29952f08a7f0a15381d0ba5e71e
Marco <marcosteam7@gmail.com>
2024-03-27 17:35:47 -0400

    moved click listener into DOM listener

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 const { invoke } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog
 
-document.getElementById('fileSelect').addEventListener('click', () => {
-  const selected = open({
-    multiple: false,
-    filters: [{
-      name: 'Image',
-      extensions: ['png', 'jpeg']
-    }]
-  });
 
-  if (selected !== null) {
-    // TODO: set image display
-  }
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('fileSelect').addEventListener('click', () => {
+      const selected = open({
+        multiple: false,
+        filters: [{
+          name: 'Image',
+          extensions: ['png', ]
+        }]
+      })
+
+      if (selected !== null) {
+        // TODO: send it to rust?
+      }
+    });
+    
 });
+
