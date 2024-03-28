316464db77381a9d322ecbe2f70e249841cfc605
Marco <marcosteam7@gmail.com>
2024-03-27 21:25:07 -0400

    drop listener

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 const { invoke } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog
 
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById('fileSelect').addEventListener('click', () => {
-      const selected = open({
-        multiple: false,
-        filters: [{
-          name: 'Image',
-          extensions: ['png', ]
+  document.getElementById('fileSelect').addEventListener('click', () => {
+    const selected = open({
+      multiple: false,
+      filters: [{
+        name: 'Image',
+        extensions: ['png', 'jpeg']
         }]
-      })
+    })
 
-      if (selected !== null) {
-        // TODO: send it to rust?
-      }
-    });
-    
+    if (selected !== null) {
+      // TODO: send it to rust?
+    }
+  });
+
+  document.getElementById('main').addEventListener('drop', (e) => {
+    e.preventDefault();
+    console.log('hi');
+  });
+
 });
-
