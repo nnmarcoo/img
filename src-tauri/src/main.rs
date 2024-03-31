1b61f26fd237035cb7cd10d279fcde3fbf59d8a5
Marco <marcosteam7@gmail.com>
2024-03-31 01:12:56 -0400

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
