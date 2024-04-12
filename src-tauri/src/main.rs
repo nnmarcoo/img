e6f4e9e386d6305ed7e3b96f2353c73ab30c4ed8
Marco <marcosteam7@gmail.com>
2024-04-12 10:26:02 -0400

    read image

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 static mut IMAGE_PATH: String = String::new();
 
@@ -17,12 +19,20 @@ fn set_image_path(path: String) {
     }
 }
 
+#[tauri::command]
+fn read_image(path: String) -> Vec<u8> {
+    let mut file = File::open(path).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    buffer
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(args);
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![show_window, set_image_path])
+        .invoke_handler(tauri::generate_handler![show_window, set_image_path, read_image])
         .run(tauri::generate_context!())
         .expect("failed to run img");
 }
