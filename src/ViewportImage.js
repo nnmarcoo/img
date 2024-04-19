9dead1f1e5f4ab76c361a61bbcbba711c3450578
Marco <marcosteam7@gmail.com>
2024-04-19 10:27:38 -0400

    awesome sauce

---
+++ src/ViewportImage.js
@@ -0,0 +1,44 @@
+class ViewportImage {
+  #ctx;
+
+  #image;
+  #width;
+  #height;
+  #posX;
+  #posY;
+  
+  #centerX;
+  #centerY;
+
+  constructor(ctx) {
+    this.#ctx = ctx;
+    this.#image = new Image();
+  }
+
+  setImage(image) {
+    this.#image.onload = () => {
+      this.#width = this.#image.naturalWidth;
+      this.#height = this.#image.naturalHeight;
+      this.#posX = 0;
+      this.#posY = 0;
+      this.#centerX = viewport.clientWidth/2 - this.#width/2;
+      this.#centerY = viewport.clientHeight/2 - this.#height/2;
+      this.draw();
+    };
+    this.#image.src = image;
+  }
+
+  draw() {
+    this.#ctx.drawImage(this.#image, this.#centerX + this.#posX, this.#centerY + this.#posY, this.#width, this.#height);
+  }
+
+  clear() {
+    this.#ctx.clearRect(this.#centerX + this.#posX, this.#centerY + this.#posY, this.#width, this.#height);
+  }
+
+  setCenter(x, y) {
+    this.#centerX = x/2 - this.#width/2;
+    this.#centerY = y/2 - this.#height/2;
+  }
+}
+export default ViewportImage;
--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
 const { invoke, convertFileSrc } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
 const { listen } = window.__TAURI__.event;
+import ViewportImage from './ViewportImage.js';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   invoke('show_window');
 
   const viewport = document.getElementById('viewport');
-  const c = viewport.getContext('2d');
-  const img = new Image();
+  const ctx = viewport.getContext('2d');
+  const img = new ViewportImage(ctx);
 
   const imgTypes = ['png', 'jpeg', 'jpg', 'webp'];  
 
   setCanvasSize();
 
   await listen('tauri://file-drop', (e) => {
-    img.src = convertFileSrc(e.payload[0]);
-    console.log(img.src);
-  });
-
-  img.addEventListener('load', () => {
-    c.imageSmoothingEnabled= false;
-    c.drawImage(img, 50, 50, 20000, 20000);
+    img.setImage(convertFileSrc(e.payload[0]))
+    img.animate();
   });
 
   window.addEventListener('resize', () => {
     setCanvasSize();
+    img.setCenter(viewport.clientWidth, viewport.clientHeight);
+    img.draw();
   });
 
   function setCanvasSize() {
