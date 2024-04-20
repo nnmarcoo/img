1b8dd62043ce18b40f418ed5e83380b79429d04e
Marco <marcosteam7@gmail.com>
2024-04-19 20:26:57 -0400

    t

--- src/main.js
+++ src/main.js
@@ -7,22 +7,20 @@ document.addEventListener('DOMContentLoaded', async () => {
   invoke('show_window');
 
   const canvas = document.getElementById('viewport');
-  const ctx = canvas.getContext('2d');
-  const img = new Viewport(ctx);
+  const viewport = new Viewport(canvas);
 
   const imgTypes = ['png', 'jpeg', 'jpg', 'webp'];  
 
   setCanvasSize();
 
   await listen('tauri://file-drop', (e) => {
-    img.setImage(convertFileSrc(e.payload[0]))
-    img.animate();
+    viewport.setImage(convertFileSrc(e.payload[0]))
   });
 
   window.addEventListener('resize', () => {
     setCanvasSize();
-    img.setCenter(canvas.clientWidth, canvas.clientHeight);
-    img.draw();
+    viewport.setCenter();
+    viewport.draw();
   });
 
   function setCanvasSize() {
--- src/viewport.js
+++ src/viewport.js
@@ -1,4 +1,11 @@
+const zoomSteps = [ 0.05, 0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.60,
+                      0.70, 0.80, 0.90, 1.00, 1.25, 1.50, 1.75, 2.00,
+                      2.50, 3.00, 3.50, 4.00, 5.00, 6.00, 7.00, 8.00, 
+                      10.0, 12.0, 15.0, 18.0, 21.0, 25.0, 30.0, 35.0 ];
+  let zoomStep = 0;
+
 class Viewport {
+  #canvas;
   #ctx;
 
   #image;
@@ -10,19 +17,21 @@ class Viewport {
   #centerX;
   #centerY;
 
-  constructor(ctx) {
-    this.#ctx = ctx;
+  constructor(canvas) {
+    this.#canvas = canvas;
+    this.#ctx = this.#canvas.getContext('2d');
     this.#image = new Image();
   }
 
   setImage(image) {
     this.#image.onload = () => {
+      this.clear();
       this.#width = this.#image.naturalWidth;
       this.#height = this.#image.naturalHeight;
       this.#posX = 0;
       this.#posY = 0;
-      this.#centerX = viewport.clientWidth/2 - this.#width/2;
-      this.#centerY = viewport.clientHeight/2 - this.#height/2;
+      this.#centerX = this.#canvas.clientWidth/2 - this.#width/2;
+      this.#centerY = this.#canvas.clientHeight/2 - this.#height/2;
       this.draw();
     };
     this.#image.src = image;
@@ -36,9 +45,9 @@ class Viewport {
     this.#ctx.clearRect(this.#centerX + this.#posX, this.#centerY + this.#posY, this.#width, this.#height);
   }
 
-  setCenter(x, y) {
-    this.#centerX = x/2 - this.#width/2;
-    this.#centerY = y/2 - this.#height/2;
+  setCenter() {
+    this.#centerX = canvas.clientWidth/2 - this.#width/2;
+    this.#centerY = canvas.clientHeight/2 - this.#height/2;
   }
   
 }
