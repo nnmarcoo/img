b4c368c4d1ea6e36936976fac726a63e50f910b5
Marco <marcosteam7@gmail.com>
2024-04-21 12:55:18 -0400

    refactor progress

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import Viewport from './viewport.js';
 // TODO: Scrolling over zoom text should care about margins?
 // TODO: Handle multiple objects in viewport
 // Should elements that are only referenced once not be saved as const?
-// BUG: If image is deleted while being viewed, rust crashes
-
 
 document.addEventListener('DOMContentLoaded', async () => {
   invoke('show_window');
@@ -20,7 +18,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   const imgTypes = await invoke('get_image_types');
 
   viewport.fillParent();
+  viewport.draw();
  
+  canvas.addEventListener('click', selectFile);
 
   await listen('tauri://file-drop', (e) => {
     let extension = e.payload[0].substring(e.payload[0].lastIndexOf('.') + 1); // better way?
@@ -32,5 +32,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     viewport.fillParent();
     viewport.draw();
   });
+
+  async function selectFile() {
+    const file = await open({
+      multiple: false,
+      filters: [{
+        name: 'Image',
+        extensions: imgTypes
+        }]
+    })
+
+    if (file !== null) {
+      viewport.setImage(convertFileSrc(file));
+      canvas.removeEventListener('click', selectFile);
+    }
+  }
   
 });
--- src/viewport.js
+++ src/viewport.js
@@ -1,12 +1,14 @@
 const zoomSteps = [ 0.05, 0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.60,
-                      0.70, 0.80, 0.90, 1.00, 1.25, 1.50, 1.75, 2.00,
-                      2.50, 3.00, 3.50, 4.00, 5.00, 6.00, 7.00, 8.00, 
-                      10.0, 12.0, 15.0, 18.0, 21.0, 25.0, 30.0, 35.0 ];
+                    0.70, 0.80, 0.90, 1.00, 1.25, 1.50, 1.75, 2.00,
+                    2.50, 3.00, 3.50, 4.00, 5.00, 6.00, 7.00, 8.00, 
+                    10.0, 12.0, 15.0, 18.0, 21.0, 25.0, 30.0, 35.0 ];
 let zoomStep = 0;
 
 export default class Viewport {
   #canvas;
   #ctx;
+  #centerX;
+  #centerY;
 
   #image;
   #width;
@@ -14,33 +16,63 @@ export default class Viewport {
   #posX;
   #posY;
   
-  #centerX;
-  #centerY;
-
   constructor(canvas) {
     this.#canvas = canvas;
     this.#ctx = this.#canvas.getContext('2d');
     this.#image = new Image();
+
+    this.#canvas.addEventListener('click', this.#click.bind(this));
   }
 
+  #click(e) {
+
+  }
+
+
   setImage(image) {
+    this.renderLoading();
     this.#image.onload = () => {
-      this.clear();
+      this.clearImage();
       this.#width = this.#image.naturalWidth;
       this.#height = this.#image.naturalHeight;
       this.centerImage();
-      this.#centerX = this.#canvas.clientWidth/2 - this.#width/2;
-      this.#centerY = this.#canvas.clientHeight/2 - this.#height/2;
+      this.#setCenter();
       this.draw();
     };
     this.#image.src = image;
   }
 
   draw() {
+    this.#ctx.imageSmoothingEnabled = false;
+    if (this.#image.src === '') {
+      this.renderFileSelect();
+      return;
+    }
     this.#ctx.drawImage(this.#image, this.#centerX + this.#posX, this.#centerY + this.#posY, this.#width, this.#height);
   }
 
-  clear() {
+  renderFileSelect() {
+    this.#canvas.style.cursor = 'pointer';
+    this.#ctx.fillStyle = '#EEEEEE';
+    this.#ctx.textAlign = 'center';
+    // Draw icon
+    this.#ctx.font = '80px Trebuchet MS';
+    this.#ctx.fillText('🗎', canvas.clientWidth/2, canvas.clientHeight/2 + 14);
+    // Draw text
+    this.#ctx.font = '16px Trebuchet MS';
+    this.#ctx.fillText('Drag or select file to begin', canvas.clientWidth/2, canvas.clientHeight/2 + 41);
+  }
+
+  renderLoading() {
+    this.#canvas.style.cursor = 'default';
+    this.#ctx.clearRect(0, 0, this.#canvas.clientWidth, this.#canvas.clientHeight);
+    this.#ctx.fillStyle = '#EEEEEE';
+    this.#ctx.textAlign = 'center';
+    this.#ctx.font = '16px Trebuchet MS';
+    this.#ctx.fillText('Loading...', canvas.clientWidth/2, canvas.clientHeight/2 + 8);
+  }
+
+  clearImage() {
     this.#ctx.clearRect(this.#centerX + this.#posX, this.#centerY + this.#posY, this.#width, this.#height);
   }
 
