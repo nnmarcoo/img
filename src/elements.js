d607e284f38623e08911901176e51842121ed935
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-10-26 11:15:09 -0400

    boiler

--- src/elements.js
+++
@@ -1,9 +0,0 @@
-export const nextImage = document.getElementById('next-image');
-export const prevImage = document.getElementById('prev-image');
-export const zoomText = document.getElementById('zoom-text');
-export const zoomTextSymbol = document.getElementById('zoom-text-symbol');
-export const bottomBarRes = document.getElementById('bottom-bar-res');
-export const zoomTextGrid = document.getElementById('zoom-text-grid');
-export const fileSelect = document.getElementById('file-select');
-export const loadingText = document.getElementById('loading-text');
-export const checkerboard = document.getElementById('checkerboard');
--- src/filter.js
+++
@@ -1,6 +0,0 @@
-export class Filter {
-  constructor() {
-    const glCanvas = document.createElement('canvas');
-    const gl = glCanvas.getContext('webgl2');
-  }
-}
